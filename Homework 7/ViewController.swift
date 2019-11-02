968ffcfb7a2184f066884a7688322547030ad378
Lada Zudova <zudova.lada@mail.ru>
2019-11-02 19:37:38 +0300

    В программе есть картинка, при нажатии кнопки: Отобразить-отображается картинка. Если нажать на "Очистить" , то картинка удалится и при нажатии на "Отобразить" выведет сообщение об ошибке. По кнопке "Загрузить " - загружает картинку по URL

---
+++ Homework 7/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Homework 7
+//
+//  Created by Лада on 02/11/2019.
+//  Copyright © 2019 Лада. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
